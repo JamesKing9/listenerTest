0165c1ec829af469c1808f7ec5c77377b56cf3d8
JamesKing9 <shencheng.wan@outlook.com>
2018-01-27 18:41:28 +0800

    [Update]

--- src/java/com/cheng/GetConnListener.java
+++ src/java/com/cheng/GetConnListener.java
@@ -16,7 +16,17 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 /**
- *
+ * 
+ javax.​servlet
+public interface ServletContextListener extends EventListener
+
+Interface for receiving notification events about ServletContext lifecycle changes.
+In order to receive these notification events, the implementation class must be either declared in the deployment descriptor of the web application, annotated with WebListener, or registered via one of the addListener methods defined on ServletContext.
+Implementations of this interface are invoked at their contextInitialized(javax.servlet.ServletContextEvent) method in the order in which they have been declared, and at their contextDestroyed(javax.servlet.ServletContextEvent) method in reverse order.
+Since:
+Servlet 2.3
+See Also:
+ServletContextEvent
  * @author james
  */
 @WebListener
